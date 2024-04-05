8b4f6f1d22f1dbbcbe63bdb1174fb781f6fdbc4a
Nicholas Arnold <nicholas.w.arnold@gmail.com>
2024-04-05 14:19:19 +0000

    Completed The Challenge Exercise

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,8 +7,9 @@
 
 int main(){
     std::string str;
+    std::cout << "Enter Your Name: " << std::flush ;
     std::cin >> str;
-    std::cout << str;
+    std::cout << "Hello " + str + "!";
 
     std::cout << std::endl << std::endl;
     return (0);

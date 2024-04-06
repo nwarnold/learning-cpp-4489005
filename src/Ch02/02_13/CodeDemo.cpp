b1bb5f7c13e65b3d4ae052c49ea160bb2a71806e
Nicholas Arnold <nicholas.w.arnold@gmail.com>
2024-04-06 20:03:23 +0000

    completed chapter 2 challenge

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -8,7 +8,7 @@ int main(){
     int nums[5] = {1,23,32,24,337};
     float result; 
 
-    // Write your code here
+    result = (nums[0] + nums[1] + nums[2] + nums[3] + nums[4])/5.0f;
     
     std::cout << "The average is " << result << std::endl;
     
